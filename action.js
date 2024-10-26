7073a36d95b7ae3bbe695077acf3037bc0144956
iconbrijesh <iconbrijesh@gmail.com>
2024-10-26 17:10:35 +0530

    updated js file

--- action.js
+++ action.js
@@ -2,14 +2,21 @@ const addbtn =document.querySelector("#add-btn");
 const taskContainer = document.querySelector("#task-container");
 const inputBox = document.querySelector("#input-box");
 
-addbtn.addEventListner("click", ()=>{
+addbtn.addEventListener("click", ()=>{
     addTask();
     
 });
 
 const addTask = ()=>{
-    if(inputBox.value === ''){
+    if(inputBox.value===''){
         alert("Please write task to add it");
     }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        taskContainer.appendChild(li);
+
+    }   
+    inputBox.value='';
 };
-console.log("I am learning Github and web dev.");
+
