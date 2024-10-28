0c2ea55a58b6cb5de7d586b48a4996181d464442
iconbrijesh <iconbrijesh@gmail.com>
2024-10-28 11:05:29 +0530

    completed the js file, added all necessary function

--- action.js
+++ action.js
@@ -16,7 +16,35 @@ const addTask = ()=>{
         li.innerHTML = inputBox.value;
         taskContainer.appendChild(li);
 
+        let delTask = document.createElement("span");
+        delTask.innerHTML = "\u00d7";
+        li.appendChild(delTask);
+
     }   
     inputBox.value='';
+    saveData();
+
+
 };
 
+taskContainer.addEventListener("click", (e)=>{
+   if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData();
+   }
+   else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+   }
+
+},false);
+
+const saveData = ()=>{
+
+    localStorage.setItem("tasks", taskContainer.innerHTML);
+}
+
+const showTask = ()=>{
+    taskContainer.innerHTML = localStorage.getItem("tasks");
+}
+showTask();
